2e7b997e47182fb21a039fac51a97f1fd6d56204
Scott Weisman <sweisman18@gmail.com>
2012-09-26 00:14:33 -0500

    finished initial style for site

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,16 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.create(params[:user])
-		redirect_to new_user_path
+		@user = User.new(params[:user])
+		if @user.save
+			redirect_to user_path(@user)
+		else
+			render :new
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
 	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :inspiration1, :inspiration2, :inspiration3, :last_name, :organization, :title
+
+  validates_presence_of :first_name, message: "We need this info for your badge."
+  validates_presence_of :last_name, message: "We need this info for your badge."
+  validates_presence_of :email, message: "We need this info for your badge."
 end
