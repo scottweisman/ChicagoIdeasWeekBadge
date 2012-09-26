2112f92157a8f6267c15304e3f01a26aa3d34ead
Scott Weisman <sweisman18@gmail.com>
2012-09-26 01:32:44 -0500

    changed link color

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :inspiration1, :inspiration2, :inspiration3, :last_name, :organization, :title
 
-  validates_presence_of :first_name, message: "You forgot a little sumpin'"
-  validates_presence_of :last_name, message: "You forgot a little sumpin'"
-  validates_presence_of :email, message: "You forgot a little sumpin'"
+  validates_presence_of :first_name, message: "You forgot a little sumthin'"
+  validates_presence_of :last_name, message: "You forgot a little sumthin'"
+  validates_presence_of :email, message: "You forgot a little sumthin'"
 end
