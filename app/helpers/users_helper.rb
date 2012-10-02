ab139f57598b64373a7d233e1878256d69c8fc62
Tom Cullen <cullentj@gmail.com>
2012-10-02 13:23:36 -0500

    styled print css

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+  
+  def text_line_split(text)
+    inspirations = text.split(" ")
+    inspirations.each do |inspiration|
+      if inspiration.length > 12
+        inspiration = inspiration.insert(12, "-")
+      end
+    end
+    inspirations.join(" ")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ActiveRecord::Base
   validates_presence_of :first_name, message: "You forgot a little somthin'"
   validates_presence_of :last_name, message: "You forgot a little somthin'"
   validates_presence_of :email, message: "You forgot a little somthin'"
+  
+  validates :first_name, :length => { :maximum => 11, :too_long => "%{count} characters is the maximum allowed" }
+  validates :last_name, :length => { :maximum => 11, :too_long => "%{count} characters is the maximum allowed" }
+  validates :title, :length => { :maximum => 40, :too_long => "%{count} characters is the maximum allowed" }
+  validates :organization, :length => { :maximum => 40, :too_long => "%{count} characters is the maximum allowed" }
+  validates_format_of :email, :with => /@/
+  validates :inspiration1, :length => { :maximum => 24, :too_long => "%{count} characters is the maximum allowed" }
+  validates :inspiration2, :length => { :maximum => 24, :too_long => "%{count} characters is the maximum allowed" }
+  validates :inspiration3, :length => { :maximum => 24, :too_long => "%{count} characters is the maximum allowed" }
+  
+  
 end
