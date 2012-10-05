ebac6847f93bfb89d95b9776a0a2ca9c90783b59
Tom Cullen <cullentj@gmail.com>
2012-10-05 11:38:55 -0500

    Tweak CSS to print correctly, change h2 tag on Build a Badge

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,29 @@
 module UsersHelper
   
   def text_line_split(text)
-    inspirations = text.split(" ")
-    inspirations.each do |inspiration|
-      if inspiration.length > 12
-        inspiration = inspiration.insert(12, "-")
+    # inspirations = text.split(" ")
+    # inspirations.each do |inspiration|
+    #   if inspiration.length > 10
+    #     inspiration = inspiration.insert(10, "-")
+    #   end
+    # end
+    # inspirations.join(" ")
+    text_split = text.split(" ")
+    if text_split.length > 1
+      text_split.each do |split|
+        if split.length > 12
+          text_split.insert(12," ")
+        end
       end
+      return text_split.join(' ')
+    else
+      text = text.split(//)
+      text.each_index do |i|
+        if i%11 == 0 && i != 0
+          text.insert(i,'- ')
+        end
+      end
+      return text.join
     end
-    inspirations.join(" ")
   end
 end
